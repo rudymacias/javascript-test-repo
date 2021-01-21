2bad65efed5e60db7788d4219226c1b3f016e6c6
Rudy Macias <rudy@creekdigital.co>
2021-01-21 16:44:17 -0600

    Sample variables and if statement

---
+++ script.js
@@ -0,0 +1,16 @@
+var hello = "Hello";
+var student_number = 44;
+var student_total = 100;
+
+var student_math_total = student_total - student_number;
+
+console.log(student_math_total);
+
+if ( student_math_total > 50 ) {
+
+	var alert_message = "Wow there is " + student_math_total + " students!";
+
+	alert(alert_message);
+
+}
+
